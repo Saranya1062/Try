aeaa3b3ee771aa3b0a06a639c479dcd4578ec02f
Saranya1062 <saranya@synergyfolks.com>
2021-07-22 10:15:06 +0530

    Completed

---
+++ src/com/practice/Main.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!!");
+        int a=5;
+        int b=4;
+
+        System.out.println(a+b);
+    }
+}
